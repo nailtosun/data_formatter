105846981fb2ed7be01e8034a2b03d9eaa9fe2de
DESKTOP-6IJJOUF\nailt <nailtosun07@gmail.com>
2018-09-20 13:04:27 +0300

    xlsx_2_df eklendi

--- data_formatter/data_formatter.py
+++ data_formatter/data_formatter.py
@@ -1,6 +1,7 @@
 from builtins import int
 import pandas as pd
 from datetime import datetime
+import openpyxl
 def excel_time_2_string(excel_time):
     t = pd.to_datetime('1899-12-30') + pd.to_timedelta(excel_time,'D')
     return t.strftime('%d.%m.%Y')
@@ -30,3 +31,17 @@ def str_to_day(day):
 def difference(a,b):
     difference = str_to_day(a)-str_to_day(b)
     return int(difference)
+
+def xlsx_2_df(path, first_row_header = True):
+    wb = openpyxl.load_workbook(path)
+    sheet_names = wb.get_sheet_names()
+    df = pd.Dataframe()
+    for i in range (len(sheet_names)):
+        carrier = sheet_names[i]
+        sheet_ranges = wb[carrier]
+        df_carrier = pd.DataFrame(sheet_ranges.values)
+        df_carrier.columns = df.iloc[0]
+        df_carrier = df_carrier.iloc[1:]
+        df.append(df_carrier)
+    df.columns = df_carrier.columns
+    return df
--- tester.py
+++ tester.py
@@ -1,3 +1,27 @@
-from data_formatter import data_formatter as df
+from openpyxl import load_workbook
 import pandas as pd
-df.excel_time_2_string(40123)
+wb2 = load_workbook(r'C:\Users\nailt\Desktop\Machine-Learning-for-Solar-Energy-Prediction-master\staj-final\excel-files\kaski\demokrasi.xlsx')
+df = pd.DataFrame(wb2.values)
+import openpyxl
+print(openpyxl.__version__)
+sheet_names = wb2.get_sheet_names()
+name = sheet_names[0]
+sheet_ranges = wb2[name]
+df = pd.DataFrame(sheet_ranges.values)
+df.columns = df.iloc[0]
+df = df.iloc[1:]
+df
+def xlsx_2_df(path, first_row_header = True):
+    wb = openpyxl.load_workbook(path)
+    sheet_names = wb.get_sheet_names()
+    df = pd.DataFrame()
+    for i in range (len(sheet_names)):
+        carrier = sheet_names[i]
+        sheet_ranges = wb[carrier]
+        df_carrier = pd.DataFrame(sheet_ranges.values)
+        df_carrier.columns = df_carrier.iloc[0]
+        df_carrier = df_carrier.iloc[1:]
+        df.append(df_carrier)
+    return df
+path = r'C:\Users\nailt\Desktop\Machine-Learning-for-Solar-Energy-Prediction-master\staj-final\excel-files\kaski\demokrasi.xlsx'
+xlsx_2_df(path)
